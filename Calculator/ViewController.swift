190f98daf41f1ae053c4bc304bf11381cea41432
Allan <allan.g.galdino@outlook.com>
2020-07-13 11:09:56 -0300

    📱 auto-layouts applied

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -2,30 +2,27 @@
 //  ViewController.swift
 //  Calculator
 //
-//  Created by Angela Yu on 10/09/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
+//  Created by Allan Galdino on 10/09/2019.
+//  Copyright © 2020 GaldinoAllanCodes. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+  
+  override var preferredStatusBarStyle: UIStatusBarStyle {
+    return .lightContent
+  }
+  
+  @IBOutlet weak var displayLabel: UILabel!
+  
+  @IBAction func calcButtonPressed(_ sender: UIButton) {
+    //What should happen when a non-number button is pressed
+  }
+  
+  @IBAction func numButtonPressed(_ sender: UIButton) {
+    //What should happen when a number is entered into the keypad
     
-    @IBOutlet weak var displayLabel: UILabel!
-    
-    
-    
-    @IBAction func calcButtonPressed(_ sender: UIButton) {
-        
-        //What should happen when a non-number button is pressed
-    
-    }
-
-    
-    @IBAction func numButtonPressed(_ sender: UIButton) {
-        
-        //What should happen when a number is entered into the keypad
-    
-    }
-
+    sender.currentTitle
+  }
 }
-
